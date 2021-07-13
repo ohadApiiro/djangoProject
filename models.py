d79eedcc7c8a5056554b0a5f1d285d60085db01a
ohadApiiro <ohad@apiiro.com>
2021-07-13 13:30:05 +0300

    added not data model class

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
 from django.db import models as foo_models
 
 
+class NotDataModel:
+    pass
+
+
 class FooDataModel(foo_models.Model):
     name = foo_models.CharField(max_length=100)
     last_name = foo_models.CharField(max_length=100)
-
